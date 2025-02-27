5d75278a24404f37fa52c813975e252903009e07
王立硕 <wangls@enfi.com.cn>
2025-02-27 14:47:55 +0800

    [#] 修改了 [+] 增加了 [-] 删除 [!] 请注意 [?]

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import App from './App.vue'
 import router from '@/router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'; // 引入样式
-// import VueViewer from 'v-viewer';
-// import 'viewerjs/dist/viewer.css';
-// import VueRouter from 'vue-router'
+
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
@@ -14,12 +12,3 @@ new Vue({
   render: h => h(App),
 }).$mount('#app');
 
-// App.use(VueViewer, {
-//   url: 'data-src', // 指定 data-* 属性，用于存储原始图片 URL
-//   options: {
-//     // 这里可以配置 viewerjs 的选项
-//     inline: true, // 以 inline 的方式显示图片
-//     button: true, // 显示缩放按钮
-//     // ...其他选项
-//   },
-// });
