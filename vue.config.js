2d43af531feb7d72ea7534e2aa1d8c54c13c1db9
王立硕 <wangls@enfi.com.cn>
2025-02-28 11:55:54 +0800

    [#] 修改了 [+] 增加了 [-] 删除 [!] 请注意 [?]

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  //...
+  devServer: {
+    allowedHosts: [
+      'https://wn7107sv6078', // 允许访问的域名地址，即花生壳内网穿透的地址
+      '.vicp.fun'   // .是二级域名的通配符   
+    ],
+  },
+};
